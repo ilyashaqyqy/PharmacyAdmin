7cb63d7bc6acfd3efbbebf40877e968031450cd4
ilyashaqyqy <ilyasenaa24@gmail.com>
2024-04-07 01:35:08 +0000

    Testé avec succès la connexion à la base de données.

--- src/main/java/dao/test.java
+++ src/main/java/dao/test.java
@@ -7,12 +7,12 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ProduitDaoImpl produitDaoImpl = new ProduitDaoImpl() ;
-		//Produit p3 = new   Produit("vitamin c", 90, 500, "vittaaamiinn") ;
-		//Produit p4 = new   Produit("pitadin", 40, 70, "pitaddiinnn") ;
+		Produit p3 = new   Produit("vitamin c", 90, 500, "vittaaamiinn") ;
+		Produit p4 = new   Produit("pitadin", 40, 70, "pitaddiinnn") ;
 		
 
-		//produitDaoImpl.save(p3);
-		//produitDaoImpl.save(p4);
+		produitDaoImpl.save(p3);
+		produitDaoImpl.save(p4);
 	}
 
 }
